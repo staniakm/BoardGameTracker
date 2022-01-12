dd9e392089a550167f958087df919b3d17753cd1
Mariusz Staniak <mariusz.staniak@gmail.com>
2022-01-12 20:29:22 +0100

    add buttons to show hide event games and participants

--- app/src/main/java/com/mariusz/boardgametracker/ui/EventActivity.kt
+++ app/src/main/java/com/mariusz/boardgametracker/ui/EventActivity.kt
@@ -1,6 +1,7 @@
 package com.mariusz.boardgametracker.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +14,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class EventActivity : AppCompatActivity() {
 
+    private val TAG: String = "Event activity"
     private val eventViewModel: EventViewModel by viewModels()
     private lateinit var binding: ActivityEventBinding
     private lateinit var event: Event
@@ -34,6 +36,36 @@ class EventActivity : AppCompatActivity() {
         binding.finishEvent.setOnClickListener {
             event = eventViewModel.finishEvent(event.id)
             loadData()
+        }
+
+        binding.participants.setOnClickListener {
+            if (binding.games.visibility == View.GONE) {
+                binding.games.visibility = View.VISIBLE
+            } else {
+                binding.games.visibility = View.GONE
+                binding.fab.visibility = View.VISIBLE
+                setFabEvent {
+                    Log.i(TAG, "onCreate: add participant")
+                }
+            }
+        }
+
+        binding.games.setOnClickListener {
+            if (binding.participants.visibility == View.GONE) {
+                binding.participants.visibility = View.VISIBLE
+            } else {
+                binding.participants.visibility = View.GONE
+                binding.fab.visibility = View.VISIBLE
+                setFabEvent {
+                    Log.i(TAG, "onCreate: add game")
+                }
+            }
+        }
+    }
+
+    private fun setFabEvent(onClick: () -> Unit) {
+        binding.fab.setOnClickListener {
+            onClick()
         }
     }
 
